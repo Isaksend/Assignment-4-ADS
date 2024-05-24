0134c0b01be7a94a4e3635f25489043e7c30d659
Isakzend <islamkadyrbekov31@gmail.com>
2024-05-25 04:10:20 +0500

    Created WeightedGraph.java

---
+++ src/WeightedGraph.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Edge<V>>> map;  // Map of Vertex
+
+    public WeightedGraph() {
+        this.map = new HashMap<>();
+    }
+
+    public void addVertex(V data) {
+        map.putIfAbsent(new Vertex<>(data), new ArrayList<>());  // Adding new vertex
+    }
+
+    public void addEdge(V source, V dest, double weight) {
+        Vertex<V> v1 = new Vertex<>(source);
+        Vertex<V> v2 = new Vertex<>(dest);
+        map.get(v1).add(new Edge<>(v1, v2, weight));  // Add new edge
+        v1.addAdjacentVertex(v2, weight);  // Updating adjacent vertex
+    }
+
+    public Map<Vertex<V>, List<Edge<V>>> getMap() {
+        return map;  // return map of graph
+    }
+}
